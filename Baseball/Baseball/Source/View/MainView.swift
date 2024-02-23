cbd8d7e94a6b56e02f975544128fc3a0f662be6c
NewYorkKim <newyorkkim1030@gmail.com>
2024-02-23 22:04:56 +0900

    design: implement ticket section layout in main view

--- Baseball/Baseball/Source/View/MainView.swift
+++ Baseball/Baseball/Source/View/MainView.swift
@@ -8,27 +8,40 @@
 import SwiftUI
 
 struct MainView: View {
+    @State var sortingType = "최신순"
+    
+    private let sortingByNewLabel = "최신순"
+    private let sortingByOldLabel = "오래된순"
+    
     var body: some View {
         NavigationView {
-            VStack(spacing: 12) {
-                addTicketSection
-                    .background {
-                        RoundedRectangle(cornerRadius: 15.0)
-                            .fill(Color(UIColor.secondarySystemBackground))
+            ScrollView {
+                VStack(spacing: 12) {
+                    addTicketSection
+                        .background {
+                            RoundedRectangle(cornerRadius: 15.0)
+                                .fill(Color(UIColor.secondarySystemBackground))
+                        }
+                    
+                    ticketDisplayView
+                        .background {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color(UIColor.secondarySystemBackground))
+                        }
+                }
+                .padding(16)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Text("RUSH!")
+                            .fontWeight(.heavy)
+                            .italic()
                     }
-                
-                ticketDisplayView
-                    .background {
-                        RoundedRectangle(cornerRadius: 15)
-                            .fill(Color(UIColor.secondarySystemBackground))
-                    }
-            }
-            .padding(16)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    NavigationLink(destination: TeamSelectionView()) {
-                        Text("선호 구단")
-                            .bold()
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        NavigationLink(destination: TeamSelectionView()) {
+                            Text("선호 구단")
+                                .bold()
+                        }
                     }
                 }
             }
@@ -63,13 +76,63 @@ extension MainView {
 extension MainView {
     @ViewBuilder private var ticketDisplayView: some View {
             VStack(spacing: 12) {
-                Text("티켓")
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    Text("티켓")
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Menu {
+                        Button {
+                            sortingType = sortingByOldLabel
+                        } label: {
+                            Text(sortingByOldLabel)
+                        }
+                        
+                        Button {
+                            sortingType = sortingByNewLabel
+                        } label: {
+                            Text(sortingByNewLabel)
+                        }
+                    } label: {
+                        Label(sortingType, systemImage: "chevron.down")
+                    }
+                }
                 
-                TicketCarouselView()
+                ForEach(0..<5) { _ in
+                    smallTicket
+                }
             }
             .padding(20)
+    }
+}
+
+
+extension MainView {
+    // TODO: 컴포넌트화
+    @ViewBuilder private var smallTicket: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 4.0)
+            
+            HStack(spacing: 32) {
+                VStack(spacing: 12) {
+                    Image(.team)
+                    Text("패-오예주")
+                }
+                
+                VStack {
+                    Text("1 : 3")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                    Text("24.02.22")
+                }
+                
+                VStack(spacing: 12) {
+                    Image(.team)
+                    Text("승-유혜빈")
+                }
+            }
+            .foregroundColor(.white)
+            .padding()
+        }
     }
 }
 
