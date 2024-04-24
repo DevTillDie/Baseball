a44b4ad027ed46457d4bee3d9c558cda03803f54
NewYorkKim <newyorkkim1030@gmail.com>
2024-04-24 23:48:28 +0900

    design: implement main view when there are tickets

--- Baseball/Baseball/Source/View/MainView.swift
+++ Baseball/Baseball/Source/View/MainView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct MainView: View {
-    // TODO: 티켓 개수에 대한 임시 변수 -> Realm 연결 후 삭제
-    private let ticketCount = 0
+    // TODO: 티켓 데이터 대한 임시 변수 -> Realm 연결 후 삭제
+    private let ticketData = [TicketData(), TicketData(), TicketData(), TicketData(), TicketData()]
+//    private let ticketData: [TicketData] = []
     
     var body: some View {
         NavigationView {
@@ -17,21 +18,24 @@ struct MainView: View {
                 Color(.background)
                     .ignoresSafeArea()
                 
-                VStack(spacing: 0) {
-                    mainHeader
-                    
-                    addTicketSection
-                    
-                    
-                    divider
-                    
-                    ticketDisplayView
+                ScrollView {
+                    VStack(spacing: 0) {
+                        mainHeader
+                        
+                        addTicketSection
+                        
+                        divider
+                        
+                        ticketDisplayView
+                    }
+                    .padding(16)
                 }
-                .padding(16)
             }
         }
     }
 }
+
+// MARK: - mainHeader
 
 extension MainView {
     @ViewBuilder private var mainHeader: some View {
@@ -40,9 +44,11 @@ extension MainView {
             .fontWeight(.bold)
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.horizontal, 12)
-            .padding(.bottom, 15)
+            .padding(.bottom, 16)
     }
 }
+
+// MARK: - addTicketSection
 
 extension MainView {
     @ViewBuilder private var addTicketSection: some View {
@@ -65,6 +71,8 @@ extension MainView {
     }
 }
 
+// MARK: - divider
+
 extension MainView {
     @ViewBuilder private var divider: some View {
         Rectangle()
@@ -73,6 +81,8 @@ extension MainView {
             .padding(.bottom, 20)
     }
 }
+
+// MARK: - ticketDisplayView
 
 extension MainView {
     @ViewBuilder private var ticketDisplayView: some View {
@@ -83,30 +93,129 @@ extension MainView {
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.horizontal, 12)
             
-            ticketPreview
+            if ticketData.isEmpty {
+                noTicketView
+            } else {
+                VStack(spacing: 0) {
+                    ticketTags
+                    ticketPreviewStack
+                }
+            }
         }
     }
 }
 
+// MARK: - noTicketView
+
 extension MainView {
     //TODO: Realm 연결
-    @ViewBuilder private var ticketPreview: some View {
-        if ticketCount == 0 {
-            VStack {
-                Spacer()
-                
-                Text("아직 저장된 티켓이 없어요!")
-                Text("야구 경기를 보러 갈까요?")
-                
-                Spacer()
-                Spacer()
-            }
-            .foregroundColor(.caption)
-            .font(.system(size: 16))
-            .frame(maxHeight: .infinity)
-        } else {
-            Text("티켓 있당")
+    @ViewBuilder private var noTicketView: some View {
+        VStack {
+            Spacer(minLength: 160)
+            
+            Text("아직 저장된 티켓이 없어요!")
+            Text("야구 경기를 보러 갈까요?")
         }
+        .foregroundColor(.caption)
+        .font(.system(size: 16))
+        .frame(maxHeight: .infinity)
+    }
+}
+
+// MARK: - ticket
+
+extension MainView {
+    @ViewBuilder private var ticketTags: some View {
+        HStack {
+            Button {
+                // action
+            } label: {
+                Text("승요")
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 4)
+            }
+            .buttonStyle(.plain)
+            .background {
+                RoundedRectangle(cornerRadius: 15.0)
+                    .fill(.tag)
+            }
+            
+            Button {
+                // action
+            } label: {
+                Text("우리팀")
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 4)
+            }
+            .buttonStyle(.plain)
+            .background {
+                RoundedRectangle(cornerRadius: 15.0)
+                    .fill(.tag)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 12)
+        .padding(.bottom, 16)
+    }
+}
+
+// MARK: - ticketPreviewStack
+
+extension MainView {
+    @ViewBuilder private var ticketPreviewStack: some View {
+        ForEach(ticketData) { data in
+            VStack(spacing: 0) {
+                HStack {
+                    Text(data.ourTeam)
+                        .font(.system(size: 15))
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text(data.opponentTeam)
+                        .font(.system(size: 15))
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                .padding(.bottom, 20)
+                
+                VStack(spacing: 0) {
+                    Text("\(data.ourTeamScore) : \(data.opponentTeamScore)")
+                        .font(.system(size: 48))
+                        .fontWeight(.heavy)
+                    
+                    Text(data.date)
+                        .font(.system(size: 15))
+                        .fontWeight(.medium)
+                }
+            }
+            .foregroundColor(.white)
+            .padding(.horizontal, 14)
+            .padding(.top, 16)
+            .padding(.bottom, 28)
+            .background {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(.team)
+                    
+            }
+            .padding(.horizontal, 9)
+            .padding(.bottom, 12)
+        }
+    }
+}
+
+// MARK: - data
+
+extension MainView {
+    struct TicketData: Identifiable {
+        let id = UUID()
+        let date: String = "24.02.02"
+        let place: String = "고척"
+        let ourTeam: String = "SAMSUNG LIONS"
+        let opponentTeam: String = "LG TWINS"
+        let ourTeamScore: Int = 3
+        let opponentTeamScore: Int = 1
+        let feeling: String = "Good"
+        let review: String = "NICE"
     }
 }
 
