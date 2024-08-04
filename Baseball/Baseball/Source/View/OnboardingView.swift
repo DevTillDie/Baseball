0364d55ed4f9aac963b88ee93e369e852e2796db
NewYorkKim <newyorkkim1030@gmail.com>
2024-08-04 17:41:44 +0900

    design: modify onboarding main

--- Baseball/Baseball/Source/View/OnboardingView.swift
+++ Baseball/Baseball/Source/View/OnboardingView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct OnboardingView: View {
     @State private var isMoveToLast = false
+    @State private var isButtonVisible: Bool = true
+    @State private var timer: Timer?
     
     var body: some View {
         ZStack {
@@ -44,23 +46,65 @@ struct OnboardingView: View {
 extension OnboardingView {
     private var onboardingMain: some View {
         VStack {
+            Image(.logo)
+                .resizable()
+                .scaledToFit()
+                .padding(.horizontal, 90)
+                .padding(.top, 65)
             
             Text("간단히 기록하는\n트렌디한 야구 다이어리")
                 .foregroundStyle(.headline)
                 .bold()
                 .multilineTextAlignment(.center)
                 .padding(.top, 20)
+            
+            onboardingTags
+                .padding(.top, 54)
+            
+            Image(.baseball)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 243, height: 227)
+                .padding(.top, 40)
+            
+            Spacer()
+            
             Button { isMoveToLast.toggle() }
         label: {
             Image(.onboardingSkipButton)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 74, height: 22)
+                .opacity(isButtonVisible ? 1 : 0)
         }
+        .onAppear {
+            startBlinking()
+        }
+        .onDisappear {
+            stopBlinking()
+        }
+            
             Spacer()
         }
         .foregroundStyle(.text)
         .frame(height: UIScreen.main.bounds.height - 40)
+    }
+    
+    private var onboardingTags: some View {
+        VStack(spacing: 11) {
+            HStack {
+                OnboardingTag(content: .text("야구 기록"))
+                OnboardingTag(content: .text("# 간단"))
+                OnboardingTag(content: .image(Image(.onboardingTagArrow)))
+                OnboardingTag(content: .text("트렌디한"))
+            }
+            .frame(maxWidth: .infinity)
+            HStack {
+                OnboardingTag(content: .text(">>"))
+                OnboardingTag(content: .text("간편한 야구 다이어리"))
+                OnboardingTag(content: .text("오늘의 기록 내일"))
+            }
+        }
     }
     
     private var onboardingDescriptionOne: some View {
@@ -69,6 +113,7 @@ extension OnboardingView {
                 .font(.system(size: 24))
                 .bold()
                 .multilineTextAlignment(.center)
+            
             
             Spacer()
         }
@@ -93,6 +138,19 @@ extension OnboardingView {
         Text("가장 응원하는 구단은 어디인가요?")
             .foregroundStyle(.text)
             .frame(height: UIScreen.main.bounds.height * 1/2)
+    }
+    
+    private func startBlinking() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+            withAnimation(.easeInOut(duration: 0.5)) {
+                isButtonVisible.toggle()
+            }
+        }
+    }
+    
+    private func stopBlinking() {
+        timer?.invalidate()
+        timer = nil
     }
 }
 
