0c27cce5a5f0ca58e0921fe8c351008b6846ad13
NewYorkKim <newyorkkim1030@gmail.com>
2024-08-04 17:18:36 +0900

    chore: modify onboarding tag component

--- Baseball/Baseball/Source/Utils/Component/OnboardingTag.swift
+++ Baseball/Baseball/Source/Utils/Component/OnboardingTag.swift
@@ -7,24 +7,39 @@
 
 import SwiftUI
 
+enum OnboardingTagContent {
+    case text(String)
+    case image(Image)
+}
+
 struct OnboardingTag: View {
-    let text: String
+    let content: OnboardingTagContent
     
     var body: some View {
-        Text("\(text)")
-            .font(.system(size: 20))
-            .foregroundStyle(.onboardingTag)
-            .fixedSize()
-            .padding(.vertical, 8)
-            .padding(.horizontal, 16)
-            .background {
-                RoundedRectangle(cornerRadius: 19.5)
-                    .fill(.clear)
-                    .stroke(.onboardingTag)
+        Group {
+            switch content {
+            case .text(let text):
+                Text(text)
+                    .font(.system(size: 20))
+            case .image(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 20)
             }
+        }
+        .foregroundStyle(.onboardingTag)
+        .fixedSize()
+        .padding(.vertical, 8)
+        .padding(.horizontal, 16)
+        .background {
+            RoundedRectangle(cornerRadius: 19.5)
+                .fill(.clear)
+                .stroke(.onboardingTag)
+        }
     }
 }
 
 #Preview {
-    OnboardingTag(text: "# 간단")
+    OnboardingTag(content: .text("# 간단"))
 }
