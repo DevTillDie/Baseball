bdac978fb5cb573d4e598be2d6db824284a8dc47
NewYorkKim <newyorkkim1030@gmail.com>
2024-11-17 18:27:36 +0900

    refactor: move functions from onboarding view to onboarding view model

--- Baseball/Baseball/Source/ViewModel/OnboardingViewModel.swift
+++ Baseball/Baseball/Source/ViewModel/OnboardingViewModel.swift
@@ -10,6 +10,9 @@ import SwiftUI
 class OnboardingViewModel: ObservableObject {
     @AppStorage("myTeam") var myTeam = ""
     
+    @Published var isButtonVisible = true
+    
+    private var timer: Timer?
     let teams: [Team] = [
         Team(imageName: "lionsIcon", teamName: "삼성 라이온즈"),
         Team(imageName: "twinsIcon", teamName: "LG 트윈스"),
@@ -18,4 +21,19 @@ class OnboardingViewModel: ObservableObject {
         Team(imageName: "eaglesIcon", teamName: "한화 이글스"),
         Team(imageName: "bearsIcon", teamName: "두산 베어스")
     ]
+    
+    func startBlinking() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] _ in
+            DispatchQueue.main.async {
+                withAnimation(.easeInOut(duration: 0.5)) {
+                    self?.isButtonVisible.toggle()
+                }
+            }
+        }
+    }
+    
+    func stopBlinking() {
+        timer?.invalidate()
+        timer = nil
+    }
 }
